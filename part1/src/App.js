9a5b24f13c74f1360ff59e6b596fe58bfa235319
mohamed <mohamedabdelfatah2027@gmail.com>
2021-08-10 21:03:36 +0200

    finshed part1 until exercise 5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,30 +1,59 @@
-import './App.css';
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const course = {
+ 
+    name : 'Half Stack application development',
 
-  return (
+    parts : [
+   {
+    name : 'Fundamentals of React',
+    exercises : 10
+   },
+   {
+    name : 'Using props to pass data',
+    exercises :7 
+   },
+   {
+    name :'State of a component',
+    exercises :14 
+   }
+  ]
+}
+    return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course}/>
+      <Content course ={course}/>
+      <Total course={course}/>
     </div>
   )
+}
+
+const Header = (props) =>{
+  return(
+    <h1>
+      {props.name.name}
+    </h1>
+  )
+}
+const Content = (props) => {
+  return(
+    <div>
+      <p>{props.course.parts[0].name}  {props.course.parts[0].exercises}</p>
+      <p>{props.course.parts[1].name}  {props.course.parts[1].exercises}</p>
+      <p>{props.course.parts[2].name}  {props.course.parts[2].exercises}</p>
+    </div>
+  )
+}
+const Total = (props) => {
+  
+    return(
+      <div>
+        <p>Number of exercises {props.course.parts[0].exercises + 
+        props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+      </div>
+    )
 }
 
 export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import React from 'react'
+
 
 ReactDOM.render(
   <React.StrictMode>
